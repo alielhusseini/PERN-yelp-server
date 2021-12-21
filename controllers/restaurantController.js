274e095a080f72d4c8f891f5a8aa0eb6b8023ed9
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-12-21 14:00:04 +0200

    creating CRUD

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -6,7 +6,7 @@ module.exports.getAllRestaurants = async(req, res) => {
         // const client = await pool.connect()
         // const result = await client.query("select * from restaurants")
         // client.release()
-        const result = await db.query("select * from restaurants")
+        const result = await db.query("SELECT * FROM restaurants")
         res.status(200).json({
             status: "success",
             results: result.rows.length,
@@ -25,7 +25,7 @@ module.exports.getRestaurant = async(req, res) => {
         // const client = await pool.connect()
         // const result = await client.query('select * from restaurants where res_id = $1', [id])
         // client.release()
-        const result = await db.query('select * from restaurants where res_id = $1', [id])
+        const result = await db.query('SELECT * FROM restaurants WHERE res_id = $1', [id])
         res.status(200).json({
             status: "success",
             data: { restaurant: result.rows[0] }
@@ -36,18 +36,28 @@ module.exports.getRestaurant = async(req, res) => {
 }
 
 module.exports.postNewRestaurant = async(req, res) => {
-    const { x } = req.body
+    const { name, location, price_range } = req.body
     try {
+        const result = await db.query('INSERT INTO restaurants(res_name, res_location, res_price_range) values($1, $2, $3) returning *', [name, location, price_range])
 
+        res.status(201).json({
+            status: "success",
+            data: { restaurant: result.rows[0] }
+        })
     } catch (err) {
         res.status(400).json({ err: err.message })
     }
 }
 
 module.exports.updateRestaurant = async(req, res) => {
-    const id = req.params.id
+    const { body: { name, location, price_range }, params: { id } } = req
     try {
+        const result = await db.query('UPDATE restaurants SET res_name = $1, res_location = $2, res_price_range = $3 WHERE res_id = $4 returning *', [name, location, price_range, id])
 
+        res.status(201).json({
+            status: "success",
+            data: { restaurant: result.rows[0] }
+        })
     } catch (err) {
         res.status(400).json({ err: err.message })
     }
@@ -56,7 +66,12 @@ module.exports.updateRestaurant = async(req, res) => {
 module.exports.deleteRestaurant = async(req, res) => {
     const id = req.params.id
     try {
+        const result = await db.query('DELETE FROM restaurants WHERE res_id = $1 returning *', [id])
 
+        res.status(201).json({
+            status: "success",
+            data: { restaurant: result.rows[0] }
+        })
     } catch (err) {
         res.status(400).json({ err: err.message })
     }
